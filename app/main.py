c1b339cf6a83dfb8726ec803cc546bac08a1a4bb
José Augusto Oliveira Ferreira <104651266+jaofe@users.noreply.github.com>
2024-11-19 20:16:08 -0300

    :wrench: chore: cors configuration for deploy

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://sos-web-deploy.vercel.app/"],  # Allow localhost:3000
+    allow_origins=["*"],  # Allow localhost:3000
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
