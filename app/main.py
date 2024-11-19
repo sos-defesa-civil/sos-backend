34d2acae5391e4b1b5252b9963b3b31dc0223909
José Augusto Oliveira Ferreira <104651266+jaofe@users.noreply.github.com>
2024-11-19 20:03:59 -0300

    :wrench: chore: cors configuration for deploy

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow localhost:3000
+    allow_origins=["http://localhost:3000","https://sos-web-deploy.vercel.app/"],  # Allow localhost:3000
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
@@ -16,9 +16,9 @@ app.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(ocorrencia.router, prefix="/api", tags=["Ocorrência"])
-app.include_router(usuario.router, prefix="/api", tags=["Usuário"])
-app.include_router(feedback.router, prefix="/api", tags=["FeedBack"])
-app.include_router(midia.router, prefix="/api", tags=["Mídia"])
-app.include_router(registro.router, prefix="/api", tags=["Registro"])
-app.include_router(dashboard.router, prefix="/api", tags=["DashBoard"])
+app.include_router(ocorrencia.router, prefix="/api", tags=["ocorrencia"])
+app.include_router(usuario.router, prefix="/api", tags=["usuario"])
+app.include_router(feedback.router, prefix="/api", tags=["feedback"])
+app.include_router(midia.router, prefix="/api", tags=["midia"])
+app.include_router(registro.router, prefix="/api", tags=["registro"])
+app.include_router(dashboard.router, prefix="/api", tags=["dashboard"])
